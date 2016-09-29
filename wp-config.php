6f6d17a565b0f60b56da75dab3c3150ab600bc97
shenfu1991 <xuanyuantiandipan@163.com>
2016-09-29 13:42:57 +0800

    com
    com

--- wp-config.php
+++ wp-config.php
@@ -1,22 +1,22 @@
 <?php
 /**
-* WordPress基础配置文件。
-*
-* 这个文件被安装程序用于自动生成wp-config.php配置文件，
-* 您可以不使用网站，您需要手动复制这个文件，
-* 并重命名为“wp-config.php”，然后填入相关信息。
-*
-* 本文件包含以下配置选项：
-*
-* * MySQL设置
-* * 密钥
-* * 数据库表名前缀
-* * ABSPATH
-*
-* @link https://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
-*
-* @package WordPress
-*/
+ * WordPress基础配置文件。
+ *
+ * 这个文件被安装程序用于自动生成wp-config.php配置文件，
+ * 您可以不使用网站，您需要手动复制这个文件，
+ * 并重命名为“wp-config.php”，然后填入相关信息。
+ *
+ * 本文件包含以下配置选项：
+ *
+ * * MySQL设置
+ * * 密钥
+ * * 数据库表名前缀
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
+ *
+ * @package WordPress
+ */
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
@@ -38,59 +38,59 @@ define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 
 /**#@+
-* 身份认证密钥与盐。
-*
-* 修改为任意独一无二的字串！
-* 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
-* WordPress.org密钥生成服务}
-* 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
-*
-* @since 2.6.0
-*/
-define('AUTH_KEY',         'kJ1$)3vOCgPSjZ{_PNj$g~K) Se;0r#hHH,m3=r]l2E*U@1G58{6ns|>wG!YUco]');
-define('SECURE_AUTH_KEY',  'W,5u%, .-~Ys^aW[<Fq4kg7U.y3!nn&wrR+pzVuj#|itM63j/{Gj!UGMhF]8K; [');
-define('LOGGED_IN_KEY',    'jPX@I!A~`c6^F}DMFOxxV5La-6eti<hx9Glg@TWD@ ;I1H^k8vEM@1.r1t>@Z+UP');
-define('NONCE_KEY',        '1Hz-v9z :mZe~+8z%xf8BT:+#U*w_~w)4wS(Od?[a>Wu#410^=?>ja$hZRzY}l%p');
-define('AUTH_SALT',        'ONH4G@P~5|y}Djgu}nq]U*H`(gjOLG}G>-9^9]5c`x-!!GoH.-]CiJaoeW_mV{nf');
-define('SECURE_AUTH_SALT', '6=Lpw6QwbTg#RhzTzG<%kP~hkls6^:t(l6^SF%n4G6wI1Jxz_H(!@@R,Oc4&pT`=');
-define('LOGGED_IN_SALT',   'xxqd5g-@G!A7$5DA4myp8%*EM`}~b*n5ZCKZ/&lK-<!rjsQiw3,03Dy~i`lk{M3F');
-define('NONCE_SALT',       ',$zE5so|[|s.BF`?R$18N*N^;LUkqYvWI7K7{j?.~aYqRt ;U:h0m}X &UL)Z~fn');
+ * 身份认证密钥与盐。
+ *
+ * 修改为任意独一无二的字串！
+ * 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
+ * WordPress.org密钥生成服务}
+ * 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ')K*~C)!ZgBC)+W8YVnc/1&{^ekl`E|WWjN}L8(9zE)]Rv^$C=x2h:5Yxr8#q@VTX');
+define('SECURE_AUTH_KEY',  '0$G&bR@ ?OTos82xr$uHb^*>,f8Y/+pJ[~9jGMa~HAVvBvWLrW?UJcZPKBi=.T9i');
+define('LOGGED_IN_KEY',    'U#b&<fu:Fkg(F%u=f.=[4TkAy4`%;Y>V{3+OpqZw3Rc4~)y;M]q3xNLVV:bf^:FY');
+define('NONCE_KEY',        'yg)_PqsC5]0uH{:tk^QqX_,OXVoaiYtc](xa,c :j~Vlusd*!!#!![Ijgwg/+K$?');
+define('AUTH_SALT',        ')E_5Bz{RG]_xZ/(;6XxIm0fxe^*Ok[DBTu1cV}/vLNR|[Ak h8ZI^4pFcc.Oio(x');
+define('SECURE_AUTH_SALT', 'Nd[]h2,9=Moq(a[_S$PDIE~xj2PSE4mz*&V;wo52e|a:r4ixH`E,VE</<=*sc(fA');
+define('LOGGED_IN_SALT',   'a9Pz-Lf`^o(+E[IN{7E?mrnG*X^6biqAl}}KAD9s#+_GEAe<#ZN{khuX6TE>Kzk-');
+define('NONCE_SALT',       'OYf+^(13-57R,H3:ZrGSSYhOjYjN}!ciV9Z8UhK[Y-2~JW_fc@LS67200xeRVQpC');
 
 /**#@-*/
 
 /**
-* WordPress数据表前缀。
-*
-* 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
-* 不同的数据表前缀。前缀名只能为数字、字母加下划线。
-*/
+ * WordPress数据表前缀。
+ *
+ * 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
+ * 不同的数据表前缀。前缀名只能为数字、字母加下划线。
+ */
 $table_prefix  = 'wp_';
 
 /**
-* 开发者专用：WordPress调试模式。
-*
-* 将这个值改为true，WordPress将显示所有用于开发的提示。
-* 强烈建议插件开发者在开发环境中启用WP_DEBUG。
-*
-* 要获取其他能用于调试的信息，请访问Codex。
-*
-* @link https://codex.wordpress.org/Debugging_in_WordPress
-*/
+ * 开发者专用：WordPress调试模式。
+ *
+ * 将这个值改为true，WordPress将显示所有用于开发的提示。
+ * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
+ *
+ * 要获取其他能用于调试的信息，请访问Codex。
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
 define('WP_DEBUG', false);
 
 /**
-* zh_CN本地化设置：启用ICP备案号显示
-*
-* 可在设置→常规中修改。
-* 如需禁用，请移除或注释掉本行。
-*/
+ * zh_CN本地化设置：启用ICP备案号显示
+ *
+ * 可在设置→常规中修改。
+ * 如需禁用，请移除或注释掉本行。
+ */
 define('WP_ZH_CN_ICP_NUM', true);
 
 /* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
 
 /** WordPress目录的绝对路径。 */
 if ( !defined('ABSPATH') )
-define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** 设置WordPress变量和包含文件。 */
 require_once(ABSPATH . 'wp-settings.php');
